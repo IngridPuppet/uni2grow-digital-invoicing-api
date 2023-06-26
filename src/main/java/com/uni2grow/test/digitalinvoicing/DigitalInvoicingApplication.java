349d7ba1edcfbf339f33c930af9630abf67f3ed0
Ingrid Kamga <xingridkamga@gmail.com>
2023-06-26 09:54:11 +0100

    Initial commit

---
+++ src/main/java/com/uni2grow/test/digitalinvoicing/DigitalInvoicingApplication.java
@@ -0,0 +1,13 @@
+package com.uni2grow.test.digitalinvoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalInvoicingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalInvoicingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni2grow/test/digitalinvoicing/DigitalInvoicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni2grow.test.digitalinvoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalInvoicingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
