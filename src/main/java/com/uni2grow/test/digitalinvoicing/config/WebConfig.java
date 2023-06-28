242cc6860e67f3e3560230793743b0a489d757ad
Ingrid Kamga <xingridkamga@gmail.com>
2023-06-28 04:13:31 +0100

    Update CORS policy to proper implementation

--- src/main/java/com/uni2grow/test/digitalinvoicing/config/WebConfig.java
+++ src/main/java/com/uni2grow/test/digitalinvoicing/config/WebConfig.java
@@ -2,11 +2,9 @@ package com.uni2grow.test.digitalinvoicing.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
